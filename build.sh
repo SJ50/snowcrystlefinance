48b1174eab042af0781e6aadd70e973454549e77
Sameer Jain <sameer.jain@isxfinancial.com>
2022-10-07 21:39:55 +1100

    update

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 # rm -r node_modules/\@madmeerkat/ 
 # wget https://transfer.sh/awbvqJ/madmeerkat.zip -O madmeerkat.zip
 unzip -o madmeerkat.zip -d node_modules 
-rm -r madmeerkat.zip
+# rm -r madmeerkat.zip
