b2ff2e4f1d8dfe6b5d67630268d7f7860c86c5d8
Sameer Jain <sameer.jain@isxfinancial.com>
2022-10-07 20:33:04 +1100

    udpate

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 echo $(pwd)
 # echo $(ls -alh)
 # CI= GENERATE_SOURCEMAP=false react-scripts build
+ls node_modules | grep madmeerkat
 ls /app/node_modules | grep madmeerkat
 rm -r /app/node_modules/\@madmeerkat/ 
 wget https://transfer.sh/awbvqJ/madmeerkat.zip 
