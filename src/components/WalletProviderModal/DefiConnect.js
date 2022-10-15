258ff5eb8a3dc72d6b7034e1b1a7de373208911c
Sameer Jain <sameer.jain@isxfinancial.com>
2022-10-15 11:10:35 +1100

    update

--- src/components/WalletProviderModal/DefiConnect.js
+++ src/components/WalletProviderModal/DefiConnect.js
@@ -1,23 +1,22 @@
-import { DeFiWeb3Connector } from '@deficonnect/web3-connector';
+// import { DeFiWeb3Connector } from '@deficonnect/web3-connector';
 
-export const connector = new DeFiWeb3Connector({
-  supportedChainIds: [338],
-  appName: 'SnowCrystals Finance',
-  chainType: 'eth', // only support 'eth' for DeFiWeb3Connector
-  chainId: '338', // for eth is 1
-  rpcUrls: {
-    25: 'https://evm-cronos.crypto.org/',
-    338: 'https://evm-t3.cronos.org',
-  },
-});
-
-// import { DeFiWeb3Connector } from 'deficonnect';
 // export const connector = new DeFiWeb3Connector({
 //   supportedChainIds: [338],
-//   rpc: {
-//
-//     25: 'https://evm.cronos.org/', // cronos mainet
-//     338: 'https://evm-t3.cronos.org', // cronos testnet
+//   appName: 'SnowCrystals Finance',
+//   chainType: 'eth', // only support 'eth' for DeFiWeb3Connector
+//   chainId: '338', // for eth is 1
+//   rpcUrls: {
+//     25: 'https://evm-cronos.crypto.org/',
+//     338: 'https://evm-t3.cronos.org',
 //   },
-//   pollingInterval: 15000,
 // });
+
+import { DeFiWeb3Connector } from 'deficonnect';
+export const connector = new DeFiWeb3Connector({
+  supportedChainIds: [25, 338],
+  rpc: {
+    25: 'https://evm.cronos.org/', // cronos mainet
+    338: 'https://evm-t3.cronos.org', // cronos testnet
+  },
+  pollingInterval: 15000,
+});
